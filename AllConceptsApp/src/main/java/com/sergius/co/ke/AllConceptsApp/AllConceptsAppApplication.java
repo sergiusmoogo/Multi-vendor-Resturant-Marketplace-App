bfdc3767826895749ef2c059e71310e932e3e734
sergiusmoogo <moogoekiru@gmail.com>
2024-03-02 12:58:16 +0300

    first commit.

---
+++ AllConceptsApp/src/main/java/com/sergius/co/ke/AllConceptsApp/AllConceptsAppApplication.java
@@ -0,0 +1,13 @@
+package com.sergius.co.ke.AllConceptsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllConceptsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllConceptsAppApplication.class, args);
+	}
+
+}
---
+++ AllConceptsApp/src/test/java/com/sergius/co/ke/AllConceptsApp/AllConceptsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergius.co.ke.AllConceptsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllConceptsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
